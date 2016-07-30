26887e2727347f63ce4553e7a80e576171b0aca6
Marius Gundersen <me@mariusgundersen.net>
2016-07-30 20:02:52 +0200

    Hurray for clip-path! This hides the menu when it shouldn't be shown

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -24,27 +24,37 @@ export default class App extends React.Component {
     const cy = this.props.height - radius - gap;
 
     const items = [
-      {center: this.state.show ? 0.475 : 0, tool: 'wire'},
-      {center: this.state.show ? 0.625 : 0, tool: 'underpass'},
-      {center: this.state.show ? 0.775 : 0, tool: 'gate'}
+      {center: this.state.show ? 0.475 : 1/8, tool: 'wire'},
+      {center: this.state.show ? 0.625 : 1/8, tool: 'underpass'},
+      {center: this.state.show ? 0.775 : 1/8, tool: 'gate'}
     ];
 
     return (
       <svg width={this.props.width} height={this.props.height} viewBox={`0 0 ${this.props.width} ${this.props.height}`}>
+
+        <defs>
+          <clipPath id="radialClipPath">
+            <rect x="0" y="0" width={cx} height={this.props.height} fill="white" />
+            <rect x="0" y="0" width={this.props.width} height={cy} fill="white" />
+          </clipPath>
+        </defs>
+
         <circle cx={cx} cy={cy} r={radius} fill="white" stroke="black" strokeWidth="2" onClick={this.toggleShow} />
-        {items.map(item => (
-          <PieSector
-            key={item.tool}
-            cx={cx}
-            cy={cy}
-            innerRadius={radius+gap}
-            outerRadius={100}
-            turnFractionCenter={item.center}
-            turnFractionSection={0.15}
-            gap={2}
-            selected={this.state.selectedTool == item.tool}
-            onClick={() => this.setTool(item.tool)} />
-        ))}
+        <g clipPath="url(#radialClipPath)">
+          {items.map(item => (
+            <PieSector
+              key={item.tool}
+              cx={cx}
+              cy={cy}
+              innerRadius={radius+gap}
+              outerRadius={100}
+              turnFractionCenter={item.center}
+              turnFractionSection={0.15}
+              gap={2}
+              selected={this.state.selectedTool == item.tool}
+              onClick={() => this.setTool(item.tool)} />
+          ))}
+        </g>
       </svg>
     );
   }
