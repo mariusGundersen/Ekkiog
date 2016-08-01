5da25dda082bc7bdbc3f8512ce86c9e9fa541929
Marius Gundersen <me@mariusgundersen.net>
2016-08-02 00:22:52 +0200

    Dark theme, pretty colors

--- src/js/chargeMap/chargeMapFS.glsl
+++ src/js/chargeMap/chargeMapFS.glsl
@@ -4,17 +4,37 @@ uniform sampler2D netMap;
 uniform sampler2D netCharges;
 varying vec2 mapCoord;
 
-vec3 hsv2rgb(vec3 c);
-
 void main() {
   vec4 netCoord = texture2D(netMap, mapCoord);
   vec4 charge = texture2D(netCharges, netCoord.xy);
-  float hue = netCoord.x*255.0/7.7;
-  gl_FragColor = vec4(hsv2rgb(vec3(hue, 1.0, charge.x*0.3+0.6)), charge.x);
-}
-
-vec3 hsv2rgb(vec3 c){
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * normalize(mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y));
+  float hue = floor(mod(netCoord.x*256.0, 6.0));
+  if(netCoord.xy == vec2(0.0, 0.0)){
+    gl_FragColor = vec4(100.0/255.0, 100.0/255.0, 100.0/255.0, 1.0);
+  }else if(hue == 0.0){
+    gl_FragColor = charge.x == 1.0
+      ? vec4(245.0/255.0, 142.0/255.0, 142.0/255.0, 1.0)
+      : vec4(155.0/255.0, 74.0/255.0, 74.0/255.0, 1.0);
+  }else if(hue == 1.0){
+    gl_FragColor = charge.x == 1.0
+      ? vec4(169.0/255.0, 211.0/255.0, 171.0/255.0, 1.0)
+      : vec4(80.0/255.0, 100.0/255.0, 81.0/255.0, 1.0);
+  }else if(hue == 2.0){
+    gl_FragColor = charge.x == 1.0
+      ? vec4(254.0/255.0, 211.0/255.0, 127.0/255.0, 1.0)
+      : vec4(136.0/255.0, 94.0/255.0, 13.0/255.0, 1.0);
+  }else if(hue == 3.0){
+    gl_FragColor = charge.x == 1.0
+      ? vec4(122.0/255.0, 171.0/255.0, 212.0/255.0, 1.0)
+      : vec4(60.0/255.0, 84.0/255.0, 104.0/255.0, 1.0);
+  }else if(hue == 4.0){
+    gl_FragColor = charge.x == 1.0
+      ? vec4(214.0/255.0, 173.0/255.0, 213.0/255.0, 1.0)
+      : vec4(107.0/255.0, 69.0/255.0, 106.0/255.0, 1.0);
+  }else if(hue == 5.0){
+    gl_FragColor = charge.x == 1.0
+      ? vec4(121.0/255.0, 212.0/255.0, 213.0/255.0, 1.0)
+      : vec4(68.0/255.0, 99.0/255.0, 100.0/255.0, 1.0);
+  }else{
+    gl_FragColor = vec4(214.0/255.0, 214.0/255.0, 214.0/255.0, 1.0);
+  }
 }
