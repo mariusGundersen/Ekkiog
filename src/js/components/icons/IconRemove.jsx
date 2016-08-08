d5c9c38385460ea2f92a57add169cfc324f64b1b
Marius Gundersen <me@mariusgundersen.net>
2016-08-08 08:11:32 +0200

    Fixed icons on Firefox

--- src/js/components/icons/IconRemove.jsx
+++ src/js/components/icons/IconRemove.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default props => (
   <g>
-    <path d="m-15,-15, l30,30 m-30,0 l30,-30" stroke="#9b4a4a" strokeWidth="2" fill="none" />
+    <path d="M-15,-15 l30,30 m-30,0 l30,-30" stroke="#9b4a4a" strokeWidth="2" fill="none" />
   </g>
 );
