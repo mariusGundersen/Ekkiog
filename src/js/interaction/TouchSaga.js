35c15d83d95e2198df0a0ffe1dc9a19de30766aa
Marius Gundersen <me@mariusgundersen.net>
2016-08-23 23:28:54 +0200

    Using the revealing constructor and subclassing FTW!

--- src/js/interaction/TouchSaga.js
+++ src/js/interaction/TouchSaga.js
@@ -20,113 +20,113 @@ const MAX_UNMOVED_DISTANCE = 5;
 const MAX_TAP_TIME = 100;
 const MIN_LONG_TOUCH_TIME = 1000;
 
-export default class saga{
+export default class TouchSaga extends EventSaga {
   constructor(eventEmitter){
-    const saga = new EventSaga(eventEmitter);
-
-    saga.createOn(TOUCH_START, function(data){
-      this.data = {
-        moved: false,
-        maybeTap: true,
-        longPress: false,
-        start: {
+    super(eventEmitter, saga => {
+      saga.createOn(TOUCH_START, function(data){
+        this.data = {
+          moved: false,
+          maybeTap: true,
+          longPress: false,
+          start: {
+            x: data.x,
+            y: data.y
+          },
           x: data.x,
           y: data.y
-        },
-        x: data.x,
-        y: data.y
-      };
+        };
 
-      this.emit(POINTER_DOWN, {
-        pointer: this.id,
-        x: data.x,
-        y: data.y
+        this.emit(POINTER_DOWN, {
+          pointer: this.id,
+          x: data.x,
+          y: data.y
+        });
+
+        this.setTimeout(TAP_TOO_SLOW_TIMEOUT, MAX_TAP_TIME);
+        this.setTimeout(LONG_PRESS_TIMEOUT, {
+          x: data.x,
+          y: data.y
+        }, MIN_LONG_TOUCH_TIME);
+
       });
 
-      this.setTimeout(TAP_TOO_SLOW_TIMEOUT, MAX_TAP_TIME);
-      this.setTimeout(LONG_PRESS_TIMEOUT, {
-        x: data.x,
-        y: data.y
-      }, MIN_LONG_TOUCH_TIME);
+      saga.on(TOUCH_MOVE, function(data){
+        this.data.x = data.x;
+        this.data.y = data.y;
 
-    });
+        this.emit(POINTER_MOVE, {
+          pointer: this.id,
+          x: data.x,
+          y: data.y
+        });
 
-    saga.on(TOUCH_MOVE, function(data){
-      this.data.x = data.x;
-      this.data.y = data.y;
-
-      this.emit(POINTER_MOVE, {
-        pointer: this.id,
-        x: data.x,
-        y: data.y
-      });
-
-      if(!this.data.moved){
-        this.data.moved = Math.abs(data.x - this.data.start.x) > MAX_UNMOVED_DISTANCE
-                       || Math.abs(data.y - this.data.start.y) > MAX_UNMOVED_DISTANCE;
-        if(this.data.moved){
-          this.clearTimeout(LONG_PRESS_TIMEOUT);
-          if(!this.data.maybeTap && !this.data.longPress){
-            this.emit(POTENTIAL_LONG_PRESS_CANCEL, {
-              pointer: this.id,
-              x: data.x,
-              y: data.y
-            });
+        if(!this.data.moved){
+          this.data.moved = Math.abs(data.x - this.data.start.x) > MAX_UNMOVED_DISTANCE
+                        || Math.abs(data.y - this.data.start.y) > MAX_UNMOVED_DISTANCE;
+          if(this.data.moved){
+            this.clearTimeout(LONG_PRESS_TIMEOUT);
+            if(!this.data.maybeTap && !this.data.longPress){
+              this.emit(POTENTIAL_LONG_PRESS_CANCEL, {
+                pointer: this.id,
+                x: data.x,
+                y: data.y
+              });
+            }
           }
         }
-      }
-    });
-
-    saga.on(TAP_TOO_SLOW_TIMEOUT, function(data){
-      this.data.maybeTap = false;
-      if(this.data.moved == false){
-        this.emit(POTENTIAL_LONG_PRESS, {
-          pointer: this.id,
-          x: this.data.x,
-          y: this.data.y,
-          time: MIN_LONG_TOUCH_TIME - MAX_TAP_TIME
-        });
-      }
-    });
-
-    saga.on(LONG_PRESS_TIMEOUT, function (data) {
-      this.data.longPress = true;
-      this.emit(POINTER_UP, {
-        pointer: this.id,
-        x: data.x,
-        y: data.y
       });
 
-      this.emit(LONG_PRESS, {
-        pointer: this.id,
-        x: data.x,
-        y: data.y
+      saga.on(TAP_TOO_SLOW_TIMEOUT, function(data){
+        this.data.maybeTap = false;
+        if(this.data.moved == false){
+          this.emit(POTENTIAL_LONG_PRESS, {
+            pointer: this.id,
+            x: this.data.x,
+            y: this.data.y,
+            time: MIN_LONG_TOUCH_TIME - MAX_TAP_TIME
+          });
+        }
       });
-    });
 
-    saga.on(TOUCH_END, function(data){
-      if(this.data.maybeTap && !this.data.moved){
-        this.emit(TAP, {
-          x: data.x,
-          y: data.y
-        });
-      }
-
-      if(!this.data.maybeTap && !this.data.longPress){
-        this.emit(POTENTIAL_LONG_PRESS_CANCEL, {
+      saga.on(LONG_PRESS_TIMEOUT, function (data) {
+        this.data.longPress = true;
+        this.emit(POINTER_UP, {
           pointer: this.id,
           x: data.x,
           y: data.y
         });
-      }
 
-      this.emit(POINTER_UP, {
-        pointer: this.id,
-        x: data.x,
-        y: data.y
+        this.emit(LONG_PRESS, {
+          pointer: this.id,
+          x: data.x,
+          y: data.y
+        });
       });
 
-      this.done();
+      saga.on(TOUCH_END, function(data){
+        if(this.data.maybeTap && !this.data.moved){
+          this.emit(TAP, {
+            x: data.x,
+            y: data.y
+          });
+        }
+
+        if(!this.data.maybeTap && !this.data.longPress){
+          this.emit(POTENTIAL_LONG_PRESS_CANCEL, {
+            pointer: this.id,
+            x: data.x,
+            y: data.y
+          });
+        }
+
+        this.emit(POINTER_UP, {
+          pointer: this.id,
+          x: data.x,
+          y: data.y
+        });
+
+        this.done();
+      });
     });
   }
 }
