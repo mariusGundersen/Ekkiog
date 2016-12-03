b6290c045ba629926a2c4e87f5eeecbad1b2e335
Marius Gundersen <me@mariusgundersen.net>
2016-12-03 14:31:56 +0100

    Get the net from the component

--- src/js/editing/query/getNetAt.js
+++ src/js/editing/query/getNetAt.js
@@ -5,6 +5,7 @@ import {
   GATE,
   UNDERPASS,
   BUTTON,
+  COMPONENT,
   GROUND
 } from '../constants.js';
 
@@ -21,32 +22,44 @@ export default function getNetAt(enneaTree, x, y, dx, dy){
       return getUnderpassNet(tile, dx, dy, enneaTree, x, y);
     case BUTTON:
       return getButtonNet(tile);
+    case COMPONENT:
+      return getComponentNet(tile, dx, dy)
   }
 }
 
-export function getGateNet(gate, dx, dy){
-  if(gate.top === 1 && gate.left === 3){
-    return gate.data.net;
-  }else if(gate.top === 0 && gate.left === 0 && dx === 1 && dy === 0){
-    return gate.data.inputA.net;
-  }else if(gate.top === 2 && gate.left === 0 && dx === 1 && dy === 0){
-    return gate.data.inputB.net;
+export function getGateNet(tile, dx, dy){
+  if(tile.top === 1 && tile.left === 3){
+    return tile.data.net;
+  }else if(tile.top === 0 && tile.left === 0 && dx === 1 && dy === 0){
+    return tile.data.inputA.net;
+  }else if(tile.top === 2 && tile.left === 0 && dx === 1 && dy === 0){
+    return tile.data.inputB.net;
   }else{
     return GROUND;
   }
 }
 
-export function getUnderpassNet(underpass, dx, dy, enneaTree, x, y){
+export function getUnderpassNet(tile, dx, dy, enneaTree, x, y){
   if(dx !== 0 && dy === 0){
-    return underpass.data.net;
+    return tile.data.net;
   }else{
     return getNetAt(enneaTree, x, y+dy, dx, dy);
   }
 }
 
-export function getButtonNet(button){
-  if(button.top === 1 && button.left === 2){
-    return button.data.net;
+export function getButtonNet(tile){
+  if(tile.top === 1 && tile.left === 2){
+    return tile.data.net;
+  }else{
+    return GROUND;
+  }
+}
+
+export function getComponentNet(tile, dx, dy){
+  //TODO: add all directions
+  const output = tile.data.outputs.filter(output => output.x === tile.left && output.y === tile.top)[0];
+  if(output){
+    return output.net;
   }else{
     return GROUND;
   }
