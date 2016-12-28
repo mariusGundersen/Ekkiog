91ab2e59b5133a9b7ebe94459043f9dfee9dbb60
Marius Gundersen <me@mariusgundersen.net>
2016-12-28 15:12:55 +0100

    Added storage of components and a way to get a list of all the components

--- src/js/storage/database.js
+++ src/js/storage/database.js
@@ -1,17 +1,21 @@
 import idb from 'idb';
+import Rx from 'rxjs/Rx.js';
 
 import xor from './xor.json';
 
 import upgradeFrom0 from './upgrade/from0.js';
 import upgradeFrom1 from './upgrade/from1.js';
+import upgradeFrom2 from './upgrade/from2.js';
 
 export async function open(){
-  const db = await idb.open('ekkiog', 2, async (db) => {
+  const db = await idb.open('ekkiog', 3, async (db) => {
     switch(db.oldVersion){
       case 0:
         await upgradeFrom0(db);
       case 1:
         await upgradeFrom1(db);
+      case 2:
+        await upgradeFrom2(db);
     }
   });
 
@@ -30,5 +34,35 @@ class Storage{
   async load(){
     const result = await this.db.transaction('saves').objectStore('saves').get('default');
     return result || xor;
+  }
+
+  async saveComponent(name, forest){
+    return await this.db
+      .transaction('componets', 'readwrite')
+      .objectStore('componets')
+      .put({
+        name,
+        forest
+      });
+  }
+
+  async loadComponent(name){
+    return await this.db
+      .transaction('componets')
+      .objectStore('componets')
+      .get(name);
+  }
+
+  getComponentNames(){
+    return new Rx.Observable(s => {
+      const tx = db.transaction('components');
+      tx.objectStore('components').iterateCursor(cursor => {
+        if (!cursor) return;
+        s.next(cursor.key);
+        cursor.continue();
+      });
+      tx.complete.then(() => s.complete());
+      s.add(() => tx.abort());
+    });
   }
 }
