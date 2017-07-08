15cc8003976e4c709fe692ba7a7d49f2985f90a7
Marius Gundersen <me@mariusgundersen.net>
2017-07-08 11:50:24 +0200

    slightly better delete icon

--- src/js/components/icons/IconRemove.tsx
+++ src/js/components/icons/IconRemove.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-
 export default (props : {}) => (
-  <g>
-    <path d="M-15,-15 l30,30 m-30,0 l30,-30" stroke="#9b4a4a" strokeWidth="4" fill="none" />
+  <g transform="translate(-19, -18)">
+    <path fill="#9b4a4a" d="m31.6 6.6v3.4h-23.2v-3.4h5.7l1.8-1.6h8.2l1.8 1.6h5.7z m-21.6 25v-20h20v20c0 1.8-1.6 3.4-3.4 3.4h-13.2c-1.8 0-3.4-1.6-3.4-3.4z"></path>
   </g>
 );
