f6c9d02cd03bfc7f969a95e0ca9f2369f64e3638
Marius Gundersen <me@mariusgundersen.net>
2017-07-16 02:12:43 +0200

    fixed white border around moving items

--- src/js/engines/ViewEngine/viewFS.glsl
+++ src/js/engines/ViewEngine/viewFS.glsl
@@ -21,7 +21,13 @@ void main(void) {
   vec4 color = texture2D(spriteSheet, (spriteOffset + spriteCoord) * inverseSpriteTextureSize);
 
   if(color.a == 0.0){
-    gl_FragColor = vec4(42.0/255.0, 45.0/255.0, 48.0/255.0, 1.0);
+    if(spriteCoord.x >= tileSize/2.0-1.0
+    && spriteCoord.x <= tileSize/2.0+1.0
+    && spriteCoord.y >= tileSize/2.0-1.0
+    && spriteCoord.y <= tileSize/2.0+1.0)
+      gl_FragColor = vec4(50.0/255.0, 54.0/255.0, 58.0/255.0, 1.0);
+    else
+      gl_FragColor = vec4(42.0/255.0, 45.0/255.0, 48.0/255.0, 1.0);
   }else if(color.r == 1.0 && color.g == 0.0 && color.b == 1.0){
     vec4 charge = texture2D(chargeMap, tileCoord);
     gl_FragColor = charge;
