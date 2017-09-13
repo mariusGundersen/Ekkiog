ae6251d3f80c592cbe17bc448627ac13e7487f22
Marius Gundersen <me@mariusgundersen.net>
2017-09-13 20:35:13 +0200

    dot too big!

--- src/js/engines/ViewEngine/viewFS.glsl
+++ src/js/engines/ViewEngine/viewFS.glsl
@@ -22,9 +22,9 @@ void main(void) {
 
   if(color.a == 0.0){
     if(spriteCoord.x >= tileSize/2.0-1.0
-    && spriteCoord.x <= tileSize/2.0+1.0
+    && spriteCoord.x < tileSize/2.0+1.0
     && spriteCoord.y >= tileSize/2.0-1.0
-    && spriteCoord.y <= tileSize/2.0+1.0)
+    && spriteCoord.y < tileSize/2.0+1.0)
       gl_FragColor = vec4(50.0/255.0, 54.0/255.0, 58.0/255.0, 1.0);
     else
       gl_FragColor = vec4(42.0/255.0, 45.0/255.0, 48.0/255.0, 1.0);
