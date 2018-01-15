b03fc4707c0cea2ee93752a15060eaeb9c0173b3
Marius Gundersen <me@mariusgundersen.net>
2018-01-15 19:52:16 +0100

    return result

--- src/js/sagas/loadForest.ts
+++ src/js/sagas/loadForest.ts
@@ -19,7 +19,7 @@ export default function* loadForest({repo, name, version} : LoadForestAction) {
 export function* loadOrCreate(repo : string, name : string, version : string){
   if(repo.length === 0){
     try{
-      yield* loadOrPull(repo, name, version);
+      return yield* loadOrPull(repo, name, version);
     }catch(e){
       return {
         ...createForest(),
@@ -27,7 +27,7 @@ export function* loadOrCreate(repo : string, name : string, version : string){
       };
     }
   }else{
-    yield* loadOrPull(repo, name, version);
+    return yield* loadOrPull(repo, name, version);
   }
 }
 
