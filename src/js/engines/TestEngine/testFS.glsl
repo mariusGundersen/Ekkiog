db7b452a7f933683f8c99616ddd93ff326a39048
Marius Gundersen <me@mariusgundersen.net>
2019-11-24 18:09:42 +0100

    minor shader refactoring

--- src/js/engines/TestEngine/testFS.glsl
+++ src/js/engines/TestEngine/testFS.glsl
@@ -1,15 +1,13 @@
 precision highp float;
 
-uniform float posX;
-uniform vec2 size;
 uniform sampler2D chargeTexture;
 uniform sampler2D expectedResultTexture;
 
-varying float posY;
+varying vec2 pos;
 varying vec2 gateAddress;
 
 void main(void) {
-  float expectedResult = texture2D(expectedResultTexture, vec2(posX, posY)/size).r;
+  float expectedResult = texture2D(expectedResultTexture, pos).r;
   float actualResult = texture2D(chargeTexture, gateAddress/256.0).r;
   float success = abs(expectedResult - actualResult) < 0.6 ? 1.0 : 0.0;
   gl_FragColor = vec4(1.0 - success, success*0.7, 0.0, 0.5);
--- src/js/engines/TestEngine/testVS.glsl
+++ src/js/engines/TestEngine/testVS.glsl
@@ -5,12 +5,12 @@ uniform float posX;
 uniform vec2 size;
 
 varying vec2 gateAddress;
-varying float posY;
+varying vec2 pos;
 
 void main(void) {
-  vec2 pos = vec2(posX, position.y)/size*2.0 - 1.0;
   gateAddress = position.zw;
-  posY = position.y;
-  gl_Position = vec4(pos, 0.0, 1.0);
+  pos = vec2(posX, position.y)/size;
+
+  gl_Position = vec4(pos*2.0 - 1.0, 0.0, 1.0);
   gl_PointSize = 1.0;
 }
